697fd6bfa9956a9d6974add7b23e825487442b68
Dongin.Kim <avirtuouscircle@naver.com>
2022-11-18 16:32:38 +0900

    zip testcase update(221118)

--- src/test/java/com/example/testproject/TestprojectApplicationTests.java
+++ src/test/java/com/example/testproject/TestprojectApplicationTests.java
@@ -1,28 +1,42 @@
 package com.example.testproject;
 
-import net.lingala.zip4j.ZipFile;
-import net.lingala.zip4j.exception.ZipException;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 @SpringBootTest
+@Slf4j
 class TestprojectApplicationTests {
+
+	List<String> filesListInDir = new ArrayList<>();
 
 	@Test
 	void compressor() {
-		makeFolderToZip("C:\\Users\\white\\Desktop\\ziptest\\example.zip","C:\\Users\\white\\Desktop\\ziptest");
-		}
+		File file = new File("C:\\Users\\white\\Desktop\\ziptest\\22-10-14query.txt");
+		String zipFileName = "C:\\Users\\white\\Desktop\\ziptest\\22-10-14query.zip";
 
+		File dir = new File("C:\\Users\\white\\Desktop\\ziptest");
+		String zipDirName = "C:\\Users\\white\\Desktop\\ziptest\\ziptest.zip";
+
+		zipSingleFile(file, zipFileName);
+
+		TestprojectApplicationTests testprojectApplicationTests = new TestprojectApplicationTests();
+		testprojectApplicationTests.makeFolderToZip(dir, zipDirName);
+		}
 
 	@Test
 	void unZipTest(){
@@ -81,15 +95,75 @@ class TestprojectApplicationTests {
 		return normalizePath;
 	}
 
-	public void makeFolderToZip(String zipFileName, String folderLocation){
-		ZipFile zipFile = new ZipFile(zipFileName);
+
+
+
+
+
+	public void makeFolderToZip(File dir, String zipDirName){
 		try {
-			zipFile.addFolder(new File(folderLocation));
-		} catch (ZipException e) {
+			populateFilesList(dir);
+			//now zip files one by one
+			//create ZipOutputStream to write to the zip file
+			FileOutputStream fos = new FileOutputStream(zipDirName);
+			ZipOutputStream zos = new ZipOutputStream(fos);
+			for(String filePath : filesListInDir){
+				System.out.println("Zipping "+filePath);
+				//for ZipEntry we need to keep only relative file path, so we used substring on absolute path
+				ZipEntry ze = new ZipEntry(filePath.substring(dir.getAbsolutePath().length()+1, filePath.length()));
+				zos.putNextEntry(ze);
+				//read the file and write to ZipOutputStream
+				FileInputStream fis = new FileInputStream(filePath);
+				byte[] buffer = new byte[1024];
+				int len;
+				while ((len = fis.read(buffer)) > 0) {
+					zos.write(buffer, 0, len);
+				}
+				zos.closeEntry();
+				fis.close();
+			}
+			zos.close();
+			fos.close();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	private void populateFilesList(File dir) throws IOException {
+		File[] files = dir.listFiles();
+		for(File file : files){
+			if(file.isFile()) filesListInDir.add(file.getAbsolutePath());
+			else populateFilesList(file);
+		}
+	}
 
+	private static void zipSingleFile(File file, String zipFileName) {
+		try {
+			//create ZipOutputStream to write to the zip file
+			FileOutputStream fos = new FileOutputStream(zipFileName);
+			ZipOutputStream zos = new ZipOutputStream(fos);
+			//add a new Zip Entry to the ZipOutputStream
+			ZipEntry ze = new ZipEntry(file.getName());
+			zos.putNextEntry(ze);
+			//read the file and write to ZipOutputStream
+			FileInputStream fis = new FileInputStream(file);
+			byte[] buffer = new byte[1024];
+			int len;
+			while ((len = fis.read(buffer)) > 0) {
+				zos.write(buffer, 0, len);
+			}
 
+			//Close the zip entry to write to zip file
+			zos.closeEntry();
+			//Close resources
+			zos.close();
+			fis.close();
+			fos.close();
+			System.out.println(file.getCanonicalPath()+" is zipped to "+zipFileName);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
