d959aabc28b6bea88678ad84a81975238c888e13
Dongin.Kim <avirtuouscircle@naver.com>
2022-12-05 14:28:59 +0900

    Split file test commit

---
+++ src/test/java/com/example/testproject/SplitFileTest.java
@@ -0,0 +1,82 @@
+package com.example.testproject;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.*;
+
+@SpringBootTest
+public class SplitFileTest {
+
+    String fileName = "AppPlus-dcmcp-release.apk";
+
+    String location = "C:\\tmp\\";
+
+    String path  = location + fileName;
+
+    RandomAccessFile raf;
+
+    {
+        try {
+            raf = new RandomAccessFile(path, "r");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    long numSplits = 10; //TODO 분할 갯수 (BYTE 에 따른 분할갯수 조정 기능 추가해야됨.
+    long sourceSize;
+
+    {
+        try {
+            sourceSize = raf.length();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    long bytesPerSplit = sourceSize/numSplits ;
+    long remainingBytes = sourceSize % numSplits;
+    int maxReadBufferSize = 8 * 1024; //8KB
+
+
+    @SneakyThrows
+    @Test
+    public void splitFile(){
+
+        for(int destIx=1; destIx <= numSplits; destIx++) {
+            BufferedOutputStream bw = new BufferedOutputStream(new FileOutputStream("split."+destIx));
+            if(bytesPerSplit > maxReadBufferSize) {
+                long numReads = bytesPerSplit/maxReadBufferSize;
+                long numRemainingRead = bytesPerSplit % maxReadBufferSize;
+                for(int i=0; i<numReads; i++) {
+                    readWrite(raf, bw, maxReadBufferSize);
+                }
+                if(numRemainingRead > 0) {
+                    readWrite(raf, bw, numRemainingRead);
+                }
+            }else {
+                readWrite(raf, bw, bytesPerSplit);
+            }
+            bw.close();
+        }
+        if(remainingBytes > 0) {
+            BufferedOutputStream bw = new BufferedOutputStream(new FileOutputStream("split."+(numSplits+1)));
+            readWrite(raf, bw, remainingBytes);
+            bw.close();
+        }
+        raf.close();
+
+    }
+
+    static void readWrite(RandomAccessFile raf, BufferedOutputStream bw, long numBytes) throws IOException {
+        byte[] buf = new byte[(int) numBytes];
+        int val = raf.read(buf);
+        if(val != -1) {
+            bw.write(buf);
+        }
+    }
+
+
+}
